7fa4cdd32cf9ab91acf17b46109dafc7c8ac0759
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-03 10:51:34 -0600

    Test => define/add #recipes and #add_recipe

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -10,4 +10,33 @@ class CookBookTest < Minitest::Test
     assert_instance_of CookBook, cookbook
   end
 
+  def test_it_starts_with_no_recipes
+    cookbook = CookBook.new
+    assert_equal [], cookbook.recipes
+  end
+
+  def test_it_can_add_receipes
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    cookbook = CookBook.new
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal [recipe1, recipe2], cookbook.recipes
+  end
 end
+
+
+
+# ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+# ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+# recipe1 = Recipe.new("Mac and Cheese")
+# recipe1.add_ingredient(ingredient1, 2)
+# recipe1.add_ingredient(ingredient1, 4)
+# recipe1.add_ingredient(ingredient2, 8)
+# recipe2 = Recipe.new("Cheese Burger")
+# cookbook = CookBook.new
+#
+# cookbook.add_recipe(recipe1)
+# cookbook.add_recipe(recipe2)
