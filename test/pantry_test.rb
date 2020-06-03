3ef6521607539021074bd270ff67dd2cde1b3f56
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-03 10:20:47 -0600

    Test => define stock attribute

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -8,4 +8,9 @@ class PantryTest < Minitest::Test
     pantry = Pantry.new
     assert_instance_of Pantry, pantry
   end
+
+  def test_it_starts_with_empty_stock
+    pantry = Pantry.new
+    assert_equal ({}), pantry.stock
+  end
 end
