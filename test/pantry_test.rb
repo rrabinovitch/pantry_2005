555a1ec12b68334a787cd8f5d1f4d8f89ab89013
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-03 10:19:17 -0600

    Test => create Pantry class

---
+++ test/pantry_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
+end
