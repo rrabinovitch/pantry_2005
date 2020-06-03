7c9bb6370b6e78a7df2dc4b07965bc60a3f0780f
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-03 10:34:43 -0600

    Test => define name attr

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -14,4 +14,9 @@ class RecipeTest < Minitest::Test
     assert_instance_of Recipe, recipe1
   end
 
+  def test_it_has_name
+    recipe1 = Recipe.new("Mac and Cheese")
+    assert_equal "Mac and Cheese", recipe1.name
+  end
+
 end
