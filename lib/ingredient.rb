6e6d1f9bfff23bae7013a578969306e9f3ac5cda
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-03 10:16:44 -0600

    Test => define attributes

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,5 +1,9 @@
 class Ingredient
-  def initialize(argument)
-    @argument = argument
+  attr_reader :name, :unit, :calories
+
+  def initialize(ingredient_params)
+    @name = ingredient_params[:name]
+    @unit = ingredient_params[:unit]
+    @calories = ingredient_params[:calories]
   end
 end
