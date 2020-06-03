fa1f8dee4961ab827f7b3c5adbde422bdce71907
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-03 10:23:59 -0600

    Test => define #stock_check

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,7 +1,11 @@
 class Pantry
   attr_reader :stock
-  
+
   def initialize
     @stock = {}
+  end
+
+  def stock_check(ingredient)
+    0 if !@stock.keys.include?(ingredient.name)
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -13,4 +13,10 @@ class PantryTest < Minitest::Test
     pantry = Pantry.new
     assert_equal ({}), pantry.stock
   end
+
+  def test_it_can_check_stock
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    assert_equal 0, pantry.stock_check(ingredient1)
+  end
 end
